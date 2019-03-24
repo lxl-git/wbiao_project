43308c2bb75bf19a69e3523c5d594def34711638
lxl <949808878@qq.com>
2019-03-24 18:00:37 +0800

    registerPage eyes.js finish

---
+++ app/js/register.js
@@ -0,0 +1,11 @@
+require(['checkInput'], function(reg) {
+    reg.init();
+} )
+
+require(['eyesStatus'], function(eyes) {
+    eyes.init();
+} )
+
+require(['validationCode'], function(code) {
+    code.init();
+} )
