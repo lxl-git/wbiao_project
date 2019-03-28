6af5ce2bf30f9a9697fbc257a5e3eeae17e0d1a5
zengshoulong66 <824267576@qq.com>
2019-03-28 17:47:48 +0800

    我的

--- app/js/common.js
+++ app/js/common.js
@@ -64,3 +64,10 @@ var $top_aaa = (function(){
         }
     }
 }())
+
+$('.header').load('../pages/public.html #header_box', function() {
+    $top_aaa.init();// 公共js 执行
+});
+$('.footer').load('../pages/public.html #footer-box',function(){
+    $top_aaa.init();// 公共js 执行
+});
